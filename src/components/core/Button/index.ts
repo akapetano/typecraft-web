32be1505d2671206c3b3b3eb9d0c3f5749864aee
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2026-01-12 13:40:54 +0100

    set up Next.js project with TypeScript, PandaCSS and Park UI

---
+++ src/components/core/Button/index.ts
@@ -0,0 +1,13 @@
+export {
+  AbsoluteCenter,
+  type AbsoluteCenterProps,
+} from "./components/absolute-center";
+export {
+  Button,
+  ButtonGroup,
+  type ButtonGroupProps,
+  type ButtonProps,
+} from "./Button";
+export { Group, type GroupProps } from "./components/group";
+export { Loader, type LoaderProps } from "./components/loader";
+export { Spinner, type SpinnerProps } from "./components/spinner";
---
+++ src/theme/nextFonts.ts
@@ -0,0 +1,11 @@
+import { Geist_Mono, Space_Grotesk } from "next/font/google";
+
+export const spaceGrotesk = Space_Grotesk({
+  variable: "--font-family-space-grotesk",
+  subsets: ["latin"],
+});
+
+export const geistMono = Geist_Mono({
+  variable: "--font-family-geist-mono",
+  subsets: ["latin"],
+});
