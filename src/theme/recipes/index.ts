2e099c4c66b2ffddd0dacceabb9e4a89c61d58b2
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2026-01-13 14:54:22 +0100

    feat: create core Icon and LucideIcon components

--- src/theme/recipes/index.ts
+++ src/theme/recipes/index.ts
@@ -2,6 +2,7 @@ import { absoluteCenter } from "./absolute-center";
 import { button } from "./button";
 import { group } from "./group";
 import { heading } from "./heading";
+import { icon } from "./icon";
 import { spinner } from "./spinner";
 import { text } from "./text";
 export const recipes = {
@@ -11,5 +12,6 @@ export const recipes = {
   spinner,
   text,
   heading,
+  icon,
 };
 export const slotRecipes = {};
