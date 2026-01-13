b41bc1d028920f10e909dd4318aa1632635aff9d
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2026-01-13 23:22:30 +0100

    chore: fetch savedColorMode in layout and pass it down to RootShell, AppLayout and Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,23 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { AppLayout } from "@/components/shared/AppLayout/AppLayout";
 import { RootShell } from "@/components/shared/RootShell/RootShell";
+import { getSavedColorMode } from "@/utils/colorMode.server";
 
 export const metadata: Metadata = {
   title: "Typecraft",
   description: "Typecraft is a platform for typing practice",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const colorMode = await getSavedColorMode();
+
   return (
-    <RootShell>
-      <AppLayout>{children}</AppLayout>
+    <RootShell colorMode={colorMode}>
+      <AppLayout colorMode={colorMode}>{children}</AppLayout>
     </RootShell>
   );
 }
--- src/components/shared/RootShell/RootShell.tsx
+++ src/components/shared/RootShell/RootShell.tsx
@@ -1,16 +1,25 @@
 import { geistMono, spaceGrotesk } from "@/theme/nextFonts";
+import type { ColorMode } from "@/types/theme/colorMode";
 
 interface RootShellProps {
   children: React.ReactNode;
   lang?: string;
+  colorMode?: ColorMode;
 }
 
-export const RootShell = ({ children, lang = "en" }: RootShellProps) => {
+export const RootShell = ({
+  children,
+  lang = "en",
+  colorMode,
+}: RootShellProps) => {
   return (
-    <html lang={lang} suppressHydrationWarning>
-      <body className={`${spaceGrotesk.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html
+      lang={lang}
+      data-color-mode={colorMode}
+      className={`${spaceGrotesk.variable} ${geistMono.variable}`}
+      suppressHydrationWarning
+    >
+      <body>{children}</body>
     </html>
   );
 };
