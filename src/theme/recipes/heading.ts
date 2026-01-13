ae7419494728231a6b2181c4a4b1229d85d03a1b
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2026-01-13 16:02:01 +0100

    style: update heading recipe to set up textStyles based on as

--- src/theme/recipes/heading.ts
+++ src/theme/recipes/heading.ts
@@ -5,4 +5,29 @@ export const heading = defineRecipe({
   base: {
     fontWeight: "semibold",
   },
+  defaultVariants: {
+    size: "h2",
+  },
+  variants: {
+    size: {
+      h1: {
+        textStyle: "5xl",
+      },
+      h2: {
+        textStyle: "4xl",
+      },
+      h3: {
+        textStyle: "3xl",
+      },
+      h4: {
+        textStyle: "2xl",
+      },
+      h5: {
+        textStyle: "lg",
+      },
+      h6: {
+        textStyle: "md",
+      },
+    },
+  },
 });
