8eeae84e312b6bf1ec1b3d983489b0bb136b5033
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2026-01-15 15:41:35 +0100

    style: remove wide letterSpacing on Character, handle spacing with spaceX and marginEnd in Words

--- src/components/features/TypingTest/components/Words.tsx
+++ src/components/features/TypingTest/components/Words.tsx
@@ -1,5 +1,7 @@
+import { Fragment } from "react";
 import { Span } from "@/components/core/Span/Span";
 import { Characters } from "@/components/features/TypingTest/components/Characters";
+import { EmptyCharacter } from "@/components/features/TypingTest/components/EmptyCharacter";
 import type { CharacterState } from "@/components/features/TypingTest/TypingTest.types";
 
 interface WordsProps {
@@ -7,18 +9,23 @@ interface WordsProps {
   getCharacterState: (index: number) => CharacterState;
 }
 export function Words({ words, getCharacterState }: WordsProps) {
-  return words.map(({ word, startIndex }) => (
-    <Span
-      key={startIndex}
-      display="inline-block"
-      whiteSpace="pre"
-      marginEnd="2"
-    >
-      <Characters
-        word={word}
-        startIndex={startIndex}
-        getCharacterState={getCharacterState}
-      />
-    </Span>
+  return words.map(({ word, startIndex }, index) => (
+    <Fragment key={startIndex}>
+      <Span
+        display="inline-block"
+        whiteSpace="pre"
+        spaceX="0.5"
+        marginEnd="0.5"
+      >
+        <Characters
+          word={word}
+          startIndex={startIndex}
+          getCharacterState={getCharacterState}
+        />
+        {index < words.length - 1 && (
+          <EmptyCharacter state={getCharacterState(startIndex + word.length)} />
+        )}
+      </Span>
+    </Fragment>
   ));
 }
