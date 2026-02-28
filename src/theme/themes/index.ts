8ae3957f11e7b580b0e64bfe657a42dc1dd06da0
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2026-02-28 23:03:24 +0100

    feat(theme): add scalable multi-theme support (aurora, mono)

---
+++ src/theme/themes/index.ts
@@ -0,0 +1,7 @@
+import { auroraTheme } from "@/theme/themes/aurora";
+import { monoTheme } from "@/theme/themes/mono";
+
+export const APP_THEMES = {
+  aurora: auroraTheme,
+  mono: monoTheme,
+} as const;
