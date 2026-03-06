f176e87bf176bdc9cb1831b0342e31e53da238fb
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2026-03-06 21:48:15 +0100

    feat(Header): add ThemeSwitcher to Header

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,19 +1,25 @@
 import { Box, Container, HStack } from "styled-system/jsx";
 import { Logo } from "@/components/core/Logo/Logo";
 import { ColorModeSwitcher } from "@/components/shared/ColorModeSwitcher/ColorModeSwitcher";
+import { ThemeSwitcher } from "@/components/shared/ThemeSwitcher/ThemeSwitcher";
 import type { ColorMode } from "@/types/theme/colorMode";
+import type { Theme } from "@/types/theme/theme";
 
 interface HeaderProps {
   colorMode?: ColorMode;
+  theme?: Theme;
 }
 
-export const Header = ({ colorMode }: HeaderProps) => {
+export const Header = ({ colorMode, theme }: HeaderProps) => {
   return (
     <Box as="header" w="full" shadow="md" py="2">
       <Container maxWidth="breakpoint-xl">
         <HStack justifyContent="space-between" alignItems="center">
           <Logo />
-          <ColorModeSwitcher colorMode={colorMode} />
+          <HStack gap="2">
+            <ThemeSwitcher theme={theme} />
+            <ColorModeSwitcher colorMode={colorMode} />
+          </HStack>
         </HStack>
       </Container>
     </Box>
