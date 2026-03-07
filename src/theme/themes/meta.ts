fbfb1cba6d1c59d1bc76d31ad2d7b7801ae664bb
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2026-03-07 19:26:03 +0100

    fix(theme-meta): remove backgroundImage from meta

--- src/theme/themes/meta.ts
+++ src/theme/themes/meta.ts
@@ -3,36 +3,12 @@ import type { Theme } from "@/types/theme/theme";
 export type ThemeMeta = {
   value: Theme;
   label: string;
-  marbleBackgroundImage: string;
 };
 
 export const THEME_META: Record<Theme, ThemeMeta> = {
-  aurora: {
-    value: "aurora",
-    label: "Aurora",
-    marbleBackgroundImage: `
-      radial-gradient(circle at 28% 50%, var(--colors-mint-9) 0%, transparent 100%),
-      radial-gradient(circle at 62% 32%, var(--colors-iris-9) 0%, transparent 100%),
-      radial-gradient(circle at 52% 76%, var(--colors-sky-9) 0%, transparent 100%),
-      linear-gradient(
-        135deg,
-        var(--colors-mint-4),
-        var(--colors-iris-4),
-        var(--colors-sky-4)
-      )
-    `,
-  },
-  mono: {
-    value: "mono",
-    label: "Mono",
-    marbleBackgroundImage: `
-      linear-gradient(
-        220deg,
-        var(--colors-gray-12),
-        var(--colors-gray-2)
-      )
-    `,
-  },
+  aurora: { value: "aurora", label: "Aurora" },
+  mono: { value: "mono", label: "Mono" },
+  forest: { value: "forest", label: "Forest" },
 };
 
 export const THEME_OPTIONS = Object.values(THEME_META);
