cc36798e3806a9902188791ad8885a01a02992d2
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2026-03-07 23:29:28 +0100

    refactor(theme): change monoSharedTokens to monoTokenOverrides

--- src/theme/themes/mono/tokens/index.ts
+++ src/theme/themes/mono/tokens/index.ts
@@ -1,7 +1,7 @@
 import { radii } from "@/theme/themes/mono/tokens/radii";
 import { shadows } from "@/theme/themes/mono/tokens/shadows";
 
-export const monoSharedTokens = {
+export const monoTokenOverrides = {
   shadows: shadows,
   radii: radii,
 };
