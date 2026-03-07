e30940ba980efc0a6235a1cf2a1bdb6a5aa214d0
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2026-03-07 23:18:49 +0100

    refactor(theme): move mono to dedicated folder, and add theme-specific radii and shadows

---
+++ src/theme/themes/mono/tokens/index.ts
@@ -0,0 +1,7 @@
+import { radii } from "@/theme/themes/mono/tokens/radii";
+import { shadows } from "@/theme/themes/mono/tokens/shadows";
+
+export const monoSharedTokens = {
+  shadows: shadows,
+  radii: radii,
+};
