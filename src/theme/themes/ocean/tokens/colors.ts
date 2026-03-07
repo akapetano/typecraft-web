2ef143c1e6b14eac7a015e7639c30e122863b437
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2026-03-07 23:38:32 +0100

    refactor(theme): move ocean to dedicated folder, and add overrides for fg, border, typing.current

---
+++ src/theme/themes/ocean/tokens/colors.ts
@@ -0,0 +1,26 @@
+import { coreAppSemanticColors } from "@/theme/themes/coreSemantics";
+
+export const oceanColorOverrides = {
+  fg: {
+    ...coreAppSemanticColors.fg,
+    subtle: {
+      value: {
+        _light: "{colors.gray.9}",
+        _dark: "{colors.gray.10}",
+      },
+    },
+  },
+  border: {
+    value: {
+      _light: "{colors.gray.3}",
+      _dark: "{colors.gray.4}",
+    },
+  },
+  typing: {
+    ...coreAppSemanticColors.typing,
+    current: {
+      bg: { value: "{colors.accent.primary.a2}" },
+      underline: { value: "{colors.accent.secondary.9}" },
+    },
+  },
+} as const;
