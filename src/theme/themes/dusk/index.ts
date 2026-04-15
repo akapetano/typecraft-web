8df21f82b2f3636b91dfd2e6789779e159bc8aa8
Andrija Kapetanović <andrija.kapetanovic@gmail.com>
2026-04-15 23:29:37 +0200

    feat(theme): add shader gradient for dusk theme

--- src/theme/themes/dusk/index.ts
+++ src/theme/themes/dusk/index.ts
@@ -31,6 +31,15 @@ export const duskTheme = {
   ...duskSemantic,
   tokens: {
     gradients: {
+      shader: {
+        value: `
+          linear-gradient(180deg, var(--colors-gray-a2), transparent 25%),
+          radial-gradient(circle at 20% 20%, var(--colors-pink-a3), transparent 55%),
+          radial-gradient(circle at 80% 15%, var(--colors-orange-a3), transparent 50%),
+          radial-gradient(circle at 50% 80%, var(--colors-sky-a3), transparent 55%),
+          linear-gradient(180deg, transparent, var(--colors-sky-a1))
+          `,
+      },
       marble: {
         value:
           "radial-gradient(circle at 28% 50%, var(--colors-pink-9) 0%, transparent 100%), radial-gradient(circle at 62% 32%, var(--colors-orange-9) 0%, transparent 100%), radial-gradient(circle at 52% 76%, var(--colors-sky-9) 0%, transparent 100%), linear-gradient(135deg, var(--colors-pink-4), var(--colors-orange-4), var(--colors-sky-4))",
